51641b816e5e81322aea93161b4f0750f9dba659
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-03-12 13:25:43 +0000

    att

---
+++ src/components/CustumTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <table>
+    <thead>
+      <td>ID</td>
+      <td>Nome</td>
+    </thead>
+    <tbody>
+      <tr v-for="usuario in usuarios" :key="usuario.id">
+        <td>{{ usuario.id }}</td>
+        <td>{{ usuario.nome }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script setup lang="ts">
+defineProps<{
+  usuarios: { id: number; nome: string }[]
+}>()
+</script>
---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome {{ nome }}</h1>
+    <input  type="text" v-model="nome"/>
+    <p v-if="nome.length > 10">O nome é muito longo </p>
+
+    <CustumTable :usuarios="usuarios"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, } from 'vue'
+import CustumTable from '@/components/CustumTable.vue';
+const nome = ref('nome');
+
+const usuarios = ref([{id:1, nome:'geraldo'}, {id: 2, nome:'geraldo2'}])
+
+</script>
