c3674cf9766cd30dd5ba2b93e637b73cc38cc388
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-03-25 13:09:34 +0000

    att

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+axios.defaults.baseURL= 'https://8080-gabrielcout-springboot3-7a3qcklwx2v.ws-us110.gitpod.io/'
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/UsuarioView.vue
+++ src/views/UsuarioView.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
     <h1>Welcome {{ nome }}</h1>
-    <input  type="text" v-model="nome"/>
-    <p v-if="nome.length > 10">O nome é muito longo </p>
+    <input type="text" v-model="nome" />
+    <input type="text" v-model="senha" />
 
-    <CustumTable :usuarios="usuarios"/>
+    <p v-if="nome.length > 10">O nome é muito longo</p>
+
+    <CustumTable :usuarios="usuarios" />
+    <button type="submit" @click="novoUsuario">Clica</button>
+    <div v-if="erro">{{ erro }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, } from 'vue'
-import CustumTable from '@/components/CustumTable.vue';
-const nome = ref('nome');
+import { onMounted, ref } from 'vue'
+import CustumTable from '@/components/CustumTable.vue'
+import axios, { AxiosError } from 'axios'
+const nome = ref('')
+const senha = ref('')
+const erro = ref('')
 
-const usuarios = ref([{id:1, nome:'geraldo'}, {id: 2, nome:'geraldo2'}])
+const usuarios = ref([])
+const busca = async () => {
+  const response = await axios.get('usuario')
+  usuarios.value = response.data
+}
+const novoUsuario = async () => {
+  try {
+    usuarios.value.push(await axios.post('usuario', { nome: nome.value, senha: senha.value }))
+  } catch (error) {
+    erro.value = (error as AxiosError).message
+  }
+}
 
+onMounted(() => {
+  busca()
+})
 </script>
